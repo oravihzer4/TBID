9ace365855cf96e70150257c1fa4ed7b9cf711ed
oravihzer <oravihzer@walla.com>
2026-03-27 12:56:29 +0300

    Update base path in Vite configuration for GitHub deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Match your GitHub repo name (e.g. /repo-name/)
+  base: "/TBID/", // Match your GitHub repo name (e.g. /repo-name/)
 });
